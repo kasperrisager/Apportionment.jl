b298ba7ca287b25f758ac2ea78ca799a80b29415
kasperrisager <kasperrisager@gmail.com>
2024-04-21 12:16:10 +0200

    Write docstrings (#29)

--- src/LargestRemainders.jl
+++ src/LargestRemainders.jl
@@ -1,18 +1,43 @@
+"""
+    LargestRemainders(total_items::Int, quota)
+
+A largest remainders method apportioning `total_items` based on a `quota`.
+"""
 struct LargestRemainders
     total_items::Int
     quota
 end
 
+"""
+    Hare()
+
+The Hare quota, setting the price for each item to `sum(entitlements) / total_items`.
+"""
 struct Hare end
+
+"""
+    IntegerHare()
+
+The Hare quota, but with the price rounded up to a whole number of entitlements. Should only be used with integer entitlements.
+"""
 struct IntegerHare end
+
+"""
+    IntegerDroop()
+
+The Droop quota, setting the price for each item to `floor(1 + sum(entitlements) / (1 + total_items))`.
+Should only be used with integer entitlements.
+"""
 struct IntegerDroop end
 
 function quota(entitlements::Real, items, ::Hare)
     return entitlements / items
 end
+
 function quota(entitlements::Union{Integer, Rational}, items, ::Hare)
     return entitlements // items
 end
+
 function quota(entitlements, items, ::IntegerHare)
     return ceil(Int, quota(items, entitlements, Hare()))
 end
